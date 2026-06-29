9b1de98ca729f219af2d8dcf8f5487be70f71768
NgocQuy04 <ngoctiu010104@gmail.com>
2026-06-29 08:50:43 +0700

    API & Điều hướng cho Horse Owner

--- src/services/horseService.js
+++ src/services/horseService.js
@@ -3,6 +3,14 @@ import { API_BASE_URL } from '../constants/api'
 
 const client = axios.create({ baseURL: API_BASE_URL })
 
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
 export async function getHorses() {
   const res = await client.get('/horses')
   return res.data
